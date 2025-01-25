962dadf33b4fbebeb8d98ac10615c2c5ee49fae2
Kevin Faby <kevin@faby.fr>
2025-01-25 16:40:23 +0100

    Création d'un composant Competences

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <html>
       <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
       </head>
-      <body>
+      <body className="App">
         <Header />
         <HomePage />
       </body>
-    </div>
+    </html>
   );
 }
 
--- src/Pages/Accueil.js
+++ src/Pages/Accueil.js
@@ -1,14 +1,8 @@
+import Competences from '../Components/Competences';
 import '../CSS/Accueil.css';
 
-export default function HomePage() {
-    const competences = {
-        html5: 90,
-        css3: 80,
-        javascript: 70,
-        php: 60,
-        react: 50,
-    }
 
+export default function HomePage() {
     return (
         <div className="HomePage">
             <section>
@@ -23,28 +17,7 @@ export default function HomePage() {
                 </article>
                 <article>
                     <h3>Mes compétences</h3>
-                    <ul>
-                        <li>HTML5 {competences.html5}%</li>
-                        <div className="progress">
-                            <div className="progress-bar bg-danger" style={{width: `${competences.html5}%`}}></div>
-                        </div>
-                        <li>CSS3 {competences.css3}%</li>
-                        <div className="progress">
-                            <div className="progress-bar bg-info" style={{width: `${competences.css3}%`}}></div>
-                        </div>
-                        <li>JavaScript {competences.javascript}%</li>
-                        <div className="progress">
-                            <div className="progress-bar bg-warning" style={{width: `${competences.javascript}%`}}></div>
-                        </div>
-                        <li>PHP {competences.php}%</li>
-                        <div className="progress">
-                            <div className="progress-bar bg-success" style={{width: `${competences.php}%`}}></div>
-                        </div>
-                        <li>React {competences.react}%</li>
-                        <div className="progress">
-                            <div className="progress-bar bg-primary" style={{width: `${competences.react}%`}}></div>
-                        </div>
-                    </ul>
+                    <Competences />
                 </article>
             </section>
         </div>
